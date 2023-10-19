abe7b8ee7a4a175c929ee8f749a87dbbec25a03e
oscarEGN <oscar.egen.contact@gmail.com>
2023-10-19 17:30:58 +0200

    added the code

--- code.py
+++ code.py
@@ -1,17 +1,39 @@
+import tkinter as tk
+
 def est_bien_parenthesee(chaine):
-    stack = []  # Utilisation d'une liste comme pile
-
+    pile = []
+    ouvrantes_indices = []
     for i, char in enumerate(chaine):
+        
         if char == '(':
-            stack.append(i)  # Si c'est une parenthèse ouvrante, ajoute son indice à la pile
+            pile.append(i)
+            ouvrantes_indices.append(i)
         elif char == ')':
-            if not stack:
-                return False  # Si on trouve une parenthèse fermante sans correspondante, la chaîne est mal parenthésée
-            stack.pop()  # Si c'est une parenthèse fermante, retire la dernière parenthèse ouvrante de la pile
+            if not pile:
+                return False
+            ouvrante_index = pile.pop()
+            print(f"La parenthèse ouvrante à l'indice {ouvrante_index} correspond à la parenthèse fermante à l'indice {i}")
+    return len(pile) == 0
 
-    return len(stack) == 0  # La chaîne est bien parenthésée si la pile est vide à la fin
+def verifier_parentheses():
+    chaine = entree.get()
+    if est_bien_parenthesee(chaine):
+        resultat.config(text="La chaîne est bien parenthésée", fg="green")
+    else:
+        resultat.config(text="La chaîne n'est pas bien parenthésée", fg="red")
 
-# Exemples d'utilisation
-chaine = "()((())))"
+fenetre = tk.Tk()
+fenetre.title("Vérification de Parenthèses")
 
-print(est_bien_parenthesee(chaine))  # Renvoie False
+entree = tk.Entry(fenetre, font=("Helvetica", 14))
+entree.pack(pady=10, padx=10, ipadx=5, ipady=5, fill=tk.X)
+
+bouton = tk.Button(fenetre, text="Vérifier", command=verifier_parentheses, font=("Helvetica", 14))
+bouton.pack(pady=10)
+
+resultat = tk.Label(fenetre, text="", font=("Helvetica", 14))
+resultat.pack(pady=10)
+
+fenetre.geometry("400x200")  # Définir la taille de la fenêtre
+
+fenetre.mainloop()
